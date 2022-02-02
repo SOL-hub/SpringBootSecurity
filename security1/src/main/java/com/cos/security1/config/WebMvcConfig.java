a4bf930e5a68d225c8b7621c3dc7e749b11de423
parksol <thfdl0317@naver.com>
2022-02-02 14:33:25 +0900

    스프링부트 시큐리 설정, 로그인

---
+++ security1/src/main/java/com/cos/security1/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package com.cos.security1.config;
+
+import org.springframework.boot.web.servlet.view.MustacheViewResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer{  
+
+  @Override
+  public void configureViewResolvers(ViewResolverRegistry registry) {
+      MustacheViewResolver resolver = new MustacheViewResolver(); //오버라이딩해서 재설정
+
+      resolver.setCharset("UTF-8");//내가 너한테 던질 파일은 
+      resolver.setContentType("text/html;charset=UTF-8"); //이거야.
+      resolver.setPrefix("classpath:/templates/"); //경로의 
+      resolver.setSuffix(".html");//이러면 머스태치가 인지를 한다.
+
+      registry.viewResolver(resolver);
+  }
+}
