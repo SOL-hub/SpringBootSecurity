97689e902f2a395ca7f1825e433ab608b12607ee
parksol <thfdl0317@naver.com>
2022-02-07 17:35:53 +0900

    스프링 시큐리티 혼자 실행

--- security/src/main/java/com/sol/security2/controller/IndexController.java
+++ security/src/main/java/com/sol/security2/controller/IndexController.java
@@ -29,9 +29,9 @@ public class IndexController {
 		return "manager";
 	}
 	
-	@GetMapping("/login")
-	public @ResponseBody String login() {
-		return "login";
+	@GetMapping("/loginForm")
+	public String loginForm() {
+		return "loginForm";
 	}
 	
 	@GetMapping("/join")
---
+++ security/src/main/java/com/sol/security2/model/User.java
@@ -0,0 +1,81 @@
+package com.sol.security2.model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+	@Id//primary key
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	
+	private int id;
+	private String username;
+	private String password;
+	private String email;
+	private String role; //ROLE_USER, ROLE_ADMIN
+	
+	private String provider;
+	private String providerId;
+	//private Timestamp loginDate;
+	@CreationTimestamp
+	private Timestamp createDate;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getProvider() {
+		return provider;
+	}
+	public void setProvider(String provider) {
+		this.provider = provider;
+	}
+	public String getProviderId() {
+		return providerId;
+	}
+	public void setProviderId(String providerId) {
+		this.providerId = providerId;
+	}
+	public Timestamp getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(Timestamp createDate) {
+		this.createDate = createDate;
+	}
+	
+	
+}
