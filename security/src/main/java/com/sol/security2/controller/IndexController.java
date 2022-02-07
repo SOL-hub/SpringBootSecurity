22a724a19b3c0fffc147bc768c3b42decdd5ec39
parksol <thfdl0317@naver.com>
2022-02-08 04:29:18 +0900

    스프링 부트 시큐리티 복습

--- security/src/main/java/com/sol/security2/controller/IndexController.java
+++ security/src/main/java/com/sol/security2/controller/IndexController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,11 +30,25 @@ public class IndexController {
 	
 
 	@GetMapping("/test/login")
-	public @ResponseBody String testLogin(Authentication authentication) {
+	public @ResponseBody String testLogin(Authentication authentication,
+			@AuthenticationPrincipal PrincipalDetails userDetails) { //DI 의존성 주입
 		System.out.println("/test/login=========");
 		PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
-		System.out.println("authentication:"+principalDetails);
+		System.out.println("authentication:"+principalDetails.getUser());
+		
+		System.out.println("userDetails:"+userDetails.getUser());
 		return "세션 정보 확인하기";
+	}
+	
+	@GetMapping("/test/oauth/login")
+	public @ResponseBody String testOAuthLogin(
+			Authentication authentication, @AuthenticationPrincipal OAuth2User oauth){ //DI 의존성 주입
+		System.out.println("/test/login=========");
+		OAuth2User oauth2User = (OAuth2User) authentication.getPrincipal();
+		System.out.println("authentication:"+oauth2User.getAttributes());
+		
+		System.out.println("oauth2User:"+oauth.getAttributes());
+		return "OAuth 세션 정보 확인하기";
 	}
 	
 	@GetMapping({"","/"})
