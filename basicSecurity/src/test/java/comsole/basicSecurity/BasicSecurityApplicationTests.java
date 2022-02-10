0aa9330af5467fc587c5eb019e2c2ac8f523abb4
parksol <thfdl0317@naver.com>
2022-02-10 22:26:31 +0900

    스프링 부트 시큐리티 프로젝트 환정결성 및 초기 세팅

---
+++ basicSecurity/src/test/java/comsole/basicSecurity/BasicSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package comsole.basicSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
