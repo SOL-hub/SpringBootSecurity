1e135fbb5f361f3e24887925fca9df4eb9cf13b8
parksol <thfdl0317@naver.com>
2022-02-21 12:20:50 +0900

    Remember Me 인증

--- SpringBootSecurity/src/main/java/com/sole/security/SecurityConfig.java
+++ SpringBootSecurity/src/main/java/com/sole/security/SecurityConfig.java
@@ -8,53 +8,64 @@ import javax.naming.AuthenticationException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.AuthenticationEntryPointFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	UserDetailsService userDetailsService;
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	protected void configure(HttpSecurity http, UserDetailsService userDetailsService) throws Exception {
 		http
 		.authorizeRequests()
 		.anyRequest().authenticated();
+		
 		http
-		.formLogin()
-		.loginPage("/loginPage")
-		.defaultSuccessUrl("/")
-		.failureUrl("/loginPage")
-		.usernameParameter("userId")
-		.passwordParameter("passwd")
-		.loginProcessingUrl("/login_proc")
-		.successHandler(new AuthenticationSuccessHandler() {
+		.formLogin();
+		
+		http
+		.logout()
+		.logoutUrl("/logout")
+		.logoutSuccessUrl("/login")
+		.addLogoutHandler(new LogoutHandler() {
 			
 			@Override
-			public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-					Authentication authentication) throws IOException, ServletException {
-				System.out.println("authentication"+ authentication.getName());
-				response.sendRedirect("/");
+			public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+				HttpSession session = request.getSession();
+				session.invalidate();
 			}
 		})
-		.failureHandler(new AuthenticationFailureHandler() {
+		.logoutSuccessHandler(new LogoutSuccessHandler() {
 			
 			@Override
-			public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
-					org.springframework.security.core.AuthenticationException exception) throws IOException, ServletException {
-
-				System.out.println("excception"+ exception.getMessage());
+			public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+					throws IOException, ServletException {
 				response.sendRedirect("/login");
+				
 			}
-		}).permitAll();
+		})
+		.and()
+		.rememberMe()
+		.rememberMeParameter("remeber")
+		.tokenValiditySeconds(3600)
+		.userDetailsService(userDetailsService);
+		
 	}
 	
 }
