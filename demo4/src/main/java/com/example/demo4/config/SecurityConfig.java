0465dbd3492a66556775f46bd6925fe55734ece0
parksol <thfdl0317@naver.com>
2022-03-05 20:37:55 +0900

    시큐리티 활용

---
+++ demo4/src/main/java/com/example/demo4/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.example.demo4.config;
+
+import java.security.cert.Extension;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	//사용자 추가
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		//패스워드는 암호화된 방식으로 주어야 오류예방
+		String password = passwordEncoder().encode("1111");
+		
+		auth.inMemoryAuthentication().withUser("user").password(password).roles("USER");
+		auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER");
+		auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN");
+	}
+
+	@Bean //패스워드 암호화
+	private PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http
+		
+			.authorizeRequests()
+			.antMatchers("/", "/login**", "/List").permitAll()//인증, 권한없어도 가능하도록, 첫 화면or로그인 후or 목록조회 시 접근가능
+			.antMatchers("/List").permitAll()//목록조회에는 모두 다 접근가능
+			.antMatchers("/write/**").access("hasRole('ADMIN') or hasRole('MANAGER') or hasRole('USER)")//이 권한이 있는 사람만 들어올 수 있게한다. (글쓰기는 비회원은 접근 불가)
+			.antMatchers("/config").hasRole("ADMIN") //관리 구역
+			.anyRequest().authenticated()
+			
+		.and()
+		.formLogin();
+	}
+}
---
+++ demo4/src/main/java/com/example/demo4/controller/HomeController.java
@@ -0,0 +1,40 @@
+package com.example.demo4.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home() {
+		return "home";
+	}
+	
+	
+	//회원가입
+	@GetMapping(value = "/join")
+	public String join() {
+		return "join";
+	}
+	
+	//회원가입
+	@PostMapping(value = "/joinFinish")
+	public String joinFinish() {
+		return "joinFinish";
+	}
+	
+	//회원 목록 조회
+	@GetMapping(value = "/List")
+	public String List() {
+		return "List";
+	}
+	
+	//글쓰기
+	@GetMapping(value = "/write")
+	public String write() {
+		return "write";
+	}
+}
